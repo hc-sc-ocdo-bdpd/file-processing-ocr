8f53095b674ad58b9febae8ed4b59b098a30561e
ashah127 <aarya.shah@hc-sc.gc.ca>
2024-10-01 16:05:35 -0400

    Added flexibility to Tesseract file

--- file_processing_ocr/ocr_decorator.py
+++ file_processing_ocr/ocr_decorator.py
@@ -6,14 +6,26 @@ import pypdf
 import pytesseract
 from PIL import Image
 from file_processing_ocr.errors import OCRProcessingError, TesseractNotFound
+import os
 
 # Ensure that Tesseract is correctly located, depending on the operating system.
 try:
     pytesseract.get_tesseract_version()
 except:
     if sys.platform == 'win32':
-        pytesseract.pytesseract.tesseract_cmd = Path('C:/Users') / \
-        getpass.getuser() / 'AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
+        possible_path = [
+            "C:/Program Files/Tesseract-OCR/tesseract.exe",
+            "C:/Program Files (x86)/Tesseract-OCR/tesseract.exe",
+            Path('C:/Users') / getpass.getuser() / 'AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
+        ]
+        found = False
+        for path in possible_path:
+            if os.path.exists(path):
+                pytesseract.pytesseract.tesseract_cmd = path
+                found = True
+                break
+        if not found:
+            raise TesseractNotFound("Tesseract could not be found or is not installed.")
     elif sys.platform == 'linux':
         pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
     else:
