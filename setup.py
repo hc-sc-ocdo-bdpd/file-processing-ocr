ae526b03e1e83c77a2115090fc989de3105634d1
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2024-11-01 10:25:05 -0400

    Update setup to read from requirements file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+# Read requirements from requirements.txt
+def read_requirements():
+    with open("requirements.txt") as req_file:
+        return req_file.readlines()
 
 setup(
-    name='file-processing-ocr',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'pytesseract',
-        'Pillow',
-        'pypdf',
-    ]
+    name="file-processing-ocr",
+    version="1.0.0",
+    install_requires=read_requirements(),
 )
